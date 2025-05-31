08742707e5cfd10fa0371a0c255400b714804606
Muhammad Musa Mueed <musavuswh@gmail.com>
2025-05-31 20:33:14 +0500

    changed new logo of stay healthy

--- stay-healthy/app/page.js
+++ stay-healthy/app/page.js
@@ -1,3 +1,16 @@
+import { Button } from "antd";
+import Image from "next/image";
+
 export default function Home() {
-  return <div>Stay Healthy</div>;
+  return (
+    <div>
+      <h1>Stay Healthy</h1>
+
+      {/* Button from AntD */}
+      {/* <Button type="primary">Primary Button</Button> */}
+
+      {/* Logo */}
+      <Image src={'/logo.svg'} alt="Stay Healthy Logo" width={80} height={80} />
+    </div>
+  );
 }
