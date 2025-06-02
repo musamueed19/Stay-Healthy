040f30622790930a067592e5627f2701b19d820c
Muhammad Musa Mueed <musavuswh@gmail.com>
2025-06-02 06:57:10 +0500

    ATlast, register user, uniqueness(email) constraint, and the password encryption completed, part-2

--- stay-healthy/lib/api/users.js
+++ stay-healthy/lib/api/users.js
@@ -1,26 +1,48 @@
-"use server"
+"use server";
 
 import firestoreDatabase from "@/firebaseConfig";
-import {collection, addDoc} from "firebase/firestore"
+import { collection, addDoc, getDocs, where, query } from "firebase/firestore";
+
+// import sha256 crypto
+// import sha256 from "crypto-js/sha256";
+import CryptoJS from "crypto-js";
 
 export async function createUser(payload) {
-    try {
-        
-        const docRef = collection(firestoreDatabase, "users");
-        
-        await addDoc(docRef, payload);
+  try {
+    const docRef = collection(firestoreDatabase, "users");
 
-        return {
-            success: true,
-            message: "User created successfully."
-        }
+    // here first check, unique constraint (email)
+    const queryStmt = query(
+      collection(firestoreDatabase, "users"),
+      where("email", "==", payload.email)
+    );
 
+    const querySnapshot = await getDocs(queryStmt);
+    console.log(querySnapshot);
 
-
+    if (querySnapshot.size > 0) {
+      return {
+        success: false,
+        message: "User already exists with this email.",
+      };
     }
-    
+
+    //   Now, just preparation to add user
+    //   hash the passwod
+    const hashedPassword = CryptoJS.AES.encrypt(
+      payload.password,
+      process.env.HASHKEY
+    ).toString();
+    payload.password = hashedPassword;
+
+    await addDoc(docRef, payload);
+
+    return {
+      success: true,
+      message: "User created successfully.",
+    };
+  } catch (error) {
     // if, got error during firebase - connection, return here
-    catch (error) {
-        return {success: false, message:error};
-    }
+    return { success: false, message: error };
+  }
 }
